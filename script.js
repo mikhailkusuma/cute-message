1dc532bb00637ba654e46b1b28186184cfd3b70c
mikhailkusuma <mikhailkr2000@gmail.com>
2023-12-17 21:26:06 +0700

    Update: Thai Language

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Let's prepared we to go!";
+  question.innerHTML = "มาเตรียมพร้อมสำหรับถนนกันเถอะ!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 
